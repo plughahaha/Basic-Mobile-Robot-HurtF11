93b5772e1650f5488e59165ab0fa5e68f00fa2cb
Hurt <akkarasaknaronh@gmail.com>
2024-10-12 21:13:47 +0700

    Basic control 2 Motor (OOP)
    add function control 2 Motor (OOP)

---
+++ Mobile-Robot/MOTOR.cpp
@@ -0,0 +1,32 @@
+#include "MOTOR.h"
+
+Motor::Motor(int pwm, int dir1, int dir2) {
+  pwmPin = pwm;     
+  dirPin1 = dir1;   
+  dirPin2 = dir2;     
+
+  pinMode(pwmPin, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
+}
+
+void Motor::speed (int speed){
+    if (speed > 0){
+        digitalWrite(dirPin1,HIGH);
+        digitalWrite(dirPin2,LOW);
+    }
+    else if (speed < 0 ){
+        digitalWrite(dirPin1,LOW);
+        digitalWrite(dirPin2,HIGH);
+    }
+    else {
+        analogWrite(pwmPin,0);
+    }
+    analogWrite(pwmPin,speed);
+}
+
+void Motor::stop(){
+    analogWrite(pwmPin,0);
+    digitalWrite(dirPin1,LOW);
+    digitalWrite(dirPin2,LOW);
+}
--- Mobile-Robot/main.ino
+++ Mobile-Robot/main.ino
@@ -1,11 +1,25 @@
 #include "LED.h"
+#include "MOTOR.h"
 
 LED Led(LED_BUILTIN);  
 
+Motor motor1(9,2,3);
+Motor motor2(10,4,5);
+
 void setup() {
+  Led.turnOn();
+  delay(1000);
+  Led.turnOff();
+  delay(1000);
   
 }
 
 void loop() {
-  Led.blink(100);  
+  motor1.speed(255);
+  delay(1500);
+  motor1.stop();
+
+  motor2.speed(255);
+  delay(1500);
+  motor2.stop();
 }
