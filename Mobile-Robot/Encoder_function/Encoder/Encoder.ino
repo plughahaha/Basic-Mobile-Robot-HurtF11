a5afdac537111b208f0cefced5b7d5c7033de51e
RBH <159563081+Akkarasaknarong@users.noreply.github.com>
2024-11-11 15:02:40 +0700

    X1 X2 X4 (Raw code do not test)

--- Mobile-Robot/Encoder_function/Encoder/Encoder.ino
+++ Mobile-Robot/Encoder_function/Encoder/Encoder.ino
@@ -2,27 +2,88 @@
 #define encoder_B 16
 #define led 25
 int count_X1;
+int count_X2;
+int count_X4;
+int print;
+int last_print;
+
+int lastState_A;
+int lastState_B;
 
 void setup() {
   pinMode(encoder_A, INPUT);
   pinMode(encoder_B, INPUT);
-  pinMode(led,OUTPUT);
-  digitalWrite(led,HIGH);
-  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
 
-  attachInterrupt(digitalPinToInterrupt(encoder_A), Encoder_function, RISING);
+  lastState_A = digitalRead(encoder_A);
+  lastState_B = digitalRead(encoder_B);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(encoder_A), Encoder_function, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder_B), Encoder_function, CHANGE);
 }
 
 void loop() {
+  if (last_print != print) {
+    Serial.print("count X1 = ");
+    Serial.print(count_X1);
+    Serial.print("\t\t");
+
+    Serial.print("count X2 = ");
+    Serial.print(count_X2);
+    Serial.print("\n");
+
+    print = last_print;
+  }
 }
 
 void Encoder_function() {
-  // encoder X1
-  if (digitalRead(encoder_B) == LOW) {
-    count_X1 += 1;
-  } else {
-    count_X1 -= 1;
+  // encoder X1 ----- read just Rising of X1
+  // Rising
+  if (lastState_A == LOW && digitalRead(encoder_A) == HIGH) {
+    if (digitalRead(encoder_B) == LOW) {
+      count_X1 += 1;
+      count_X2 += 1;
+      count_X4 += 1;
+    } else {
+      count_X1 -= 1;
+      count_X2 -= 1;
+      count_X4 -= 1;
+    }
   }
-  Serial.print(count_X1);
-  
+
+  // encoder X2 ----- read Rising and Falling of X1
+  // Falling
+  if (lastState_A == HIGH && digitalRead(encoder_A) == LOW) {
+    if (digitalRead(encoder_B) == HIGH) {
+      count_X2 += 1;
+      count_X4 += 1;
+    } else {
+      count_X2 -= 1;
+      count_X4 -= 1;
+    }
+  }
+
+  // encoder X4 ----- read Rising and Falling of X1 and X2
+  // Rising
+  if (lastState_B == LOW && digitalRead(encoder_B) == HIGH) {
+    if (digitalRead(encoder_A) == HIGH) {
+      count_X4 += 1;
+    } else {
+      count_X4 -= 1;
+    }
+  }
+  // Falling
+  if (lastState_B == HIGH && digitalRead(encoder_B) == LOW) {
+    if (digitalRead(encoder_A) == LOW) {
+      count_X4 += 1;
+    } else {
+      count_X4 -= 1;
+    }
+  }
+
+
+  lastState_A = digitalRead(encoder_A);
+  lastState_B = digitalRead(encoder_B);
+  print += 1;
 }
