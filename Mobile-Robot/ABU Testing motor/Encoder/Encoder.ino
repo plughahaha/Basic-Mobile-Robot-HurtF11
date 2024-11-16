d52d95fe09b206bdac7222cd82d511d90faa998b
RBH <159563081+Akkarasaknarong@users.noreply.github.com>
2024-11-16 22:42:23 +0700

    add ball gripper

--- Mobile-Robot/ABU Testing motor/Encoder/Encoder.ino
+++ Mobile-Robot/ABU Testing motor/Encoder/Encoder.ino
@@ -6,28 +6,36 @@ Motor FR(4, 12, 13, 200, 20, 0);
 Motor BL(6, 14, 15, 200, 20, 0);
 Motor BR(8, 20, 21, 200, 20, 0);
 
+Motor Gripper(8, 10, 11, 200, 20, 0);
+
 #define led 25
 
 void setup() {
   digitalWrite(led, HIGH);
   Serial.begin(9600);
-  FL.setSpeed(255);
-  FR.setSpeed(255);
-  BL.setSpeed(255);
-  BR.setSpeed(255);
+  // FL.setSpeed(255);
+  // FR.setSpeed(255);
+  // BL.setSpeed(255);
+  // BR.setSpeed(255);
+  
 
   pinMode(led, OUTPUT);
-  for (int i = 1; i < 20; i++) {
+  for (int i = 1; i < 40; i++) {
     digitalWrite(led, HIGH);
     delay(30);
     digitalWrite(led, LOW);
     delay(30);
   }
   digitalWrite(led, HIGH);
-
 }
 
 void loop() {
-  
+  Gripper.setSpeed(100);
+  delay(650);
+  Gripper.stop();
+  delay(1000);
+  Gripper.setSpeed(-100);
+  delay(240);
+  Gripper.stop();
+  delay(1000);
 }
-
